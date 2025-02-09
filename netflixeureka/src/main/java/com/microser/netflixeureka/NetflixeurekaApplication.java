cc602ee6a12c2b2fce9f1ecddf81cf57ba0842b1
Livesh <117461978+livesh19@users.noreply.github.com>
2025-02-09 07:37:09 +0530

    Add files via upload

---
+++ netflixeureka/src/main/java/com/microser/netflixeureka/NetflixeurekaApplication.java
@@ -0,0 +1,15 @@
+package com.microser.netflixeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixeurekaApplication.class, args);
+	}
+
+
+
+}
---
+++ netflixeureka/src/test/java/com/microser/netflixeureka/NetflixeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microser.netflixeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixeurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
