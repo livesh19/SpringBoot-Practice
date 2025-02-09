4f819b3853f93c4af8325c851ab1dba7b074a79d
Livesh <117461978+livesh19@users.noreply.github.com>
2025-02-09 07:44:25 +0530

    Add files via upload

---
+++ SpringJDBC/src/main/java/com/JDBC/SpringJDBC/SpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.JDBC.SpringJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcApplication.class, args);
+	}
+
+}
